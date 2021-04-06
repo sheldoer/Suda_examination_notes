4024ea453a90e7682782f181c388ac09a5d126d9
Sheldoer <44505141+Sheldoer@users.noreply.github.com>
2021-04-06 09:25:30 +0800

    Delete 复试笔记/红果研模拟题代码 directory
    部分修改

--- 复试笔记/红果研模拟题代码/210220第七章函数.py
+++
@@ -1,4 +0,0 @@
-file=open('1.txt','r',encoding='gb18030')
-juzi=file.readlines()
-print(x for x in juzi)
-file.close()
--- 复试笔记/红果研模拟题代码/210221pdb.py
+++
@@ -1,9 +0,0 @@
-import pdb
-n=37
-pdb.set_trace()
-for i in range(2,n):
-    if n%i==0:
-        print('No')
-        break
-    else:
-        print('Yes')
